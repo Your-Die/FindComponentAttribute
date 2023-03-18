c9c01b101afbe8d6c45f4989db3e2e09a0a60576
Jordy van Opstal <jordy_van_opstal@live.nl>
2023-03-18 19:43:24 +0100

    add comments to AutoRefBehaviour

--- Runtime/AutoRefBehaviour.cs
+++ Runtime/AutoRefBehaviour.cs
@@ -21,12 +21,16 @@ namespace Chinchillada
         [Button]
         protected virtual void FindComponents()
         {
-            var attributedFields = this.GetAttributedFields();
+            var attributedFields = this.GetComponentFinderFields();
 
             foreach (var (field, attribute) in attributedFields)
                 attribute.Apply(this, field);
         }
 
+        /// <summary>
+        /// Applies the <see cref="FindComponentAttribute"/> on each <see cref="AutoRefBehaviour"/>
+        /// on the hosting <see cref="GameObject"/>.
+        /// </summary>
         [ContextMenu("Find All Components")]
         private void FindAllComponents()
         {
@@ -36,24 +40,41 @@ namespace Chinchillada
                 chinchillada.FindComponents();
         }
 
+        /// <summary>
+        /// <see cref="FindComponentsCustom"/> with <see cref="SearchStrategy.InChildren"/>.
+        /// </summary>
         [ContextMenu("Find Components In Children")]
         private void FindComponentsInChildren() => this.FindComponentsCustom(SearchStrategy.InChildren);
 
+        /// <summary>
+        /// <see cref="FindComponentsCustom"/> with <see cref="SearchStrategy.InParent"/>.
+        /// </summary>
         [ContextMenu("Find Components In parents")]
         private void FindComponentsInParents() => this.FindComponentsCustom(SearchStrategy.InParent);
 
+        /// <summary>
+        /// <see cref="FindComponentsCustom"/> with <see cref="SearchStrategy.InScene"/>.
+        /// </summary>
         [ContextMenu("Find Components Anywhere")]
         private void FindComponentsAnywhere() => this.FindComponentsCustom(SearchStrategy.InScene);
 
+        /// <summary>
+        /// Try to find components for each <see cref="FindComponentAttribute"/>, but using the
+        /// <paramref name="strategy"/>.
+        /// </summary>
         private void FindComponentsCustom(SearchStrategy strategy)
         {
-            var attributedFields = this.GetAttributedFields();
+            var attributedFields = this.GetComponentFinderFields();
 
             foreach (var (field, attribute) in attributedFields)
                 attribute.Apply(this, field, strategy);
         }
 
-        private IEnumerable<(FieldInfo, ComponentFinderAttribute)> GetAttributedFields()
+        /// <summary>
+        /// Get all fields with the <see cref="FindComponentAttribute"/>.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<(FieldInfo, ComponentFinderAttribute)> GetComponentFinderFields()
         {
             var type = this.GetType();
             return type.GetFieldsWithAttribute<ComponentFinderAttribute>();
